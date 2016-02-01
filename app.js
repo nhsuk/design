0def2cb16f0c3716b71a577c303c5e28fb31d91e
Demotive <mat@demotive.com>
2016-02-01 10:53:07 +0000

    Port fiddling

--- app.js
+++ app.js
@@ -37,6 +37,8 @@ app.get(/^\/([^.]+)$/, function (req, res) {
   });
 });
 
-app.listen(3000, function () {
+// start the app
+var port = (process.env.PORT || 3000);
+app.listen(port, function () {
   console.log('Example app listening on port 3000!');
 });
